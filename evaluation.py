1787e5c5d1f4ff39b149d2d21cf1e0d7e6584f4e
王逸飞 <wangyifei@wangyifeideMacBook-Pro.local>
2022-04-11 16:56:35 -0400

    evaluation and .gitignore added

---
+++ evaluation.py
@@ -0,0 +1,55 @@
+import cv2
+from scipy.spatial.distance import cosine
+from keras.models import Model
+import tensorflow as tf
+import numpy as np
+
+class evaluation():
+	def __init__(self):
+		vgg16 = tf.keras.applications.VGG16(weights='imagenet', include_top=True, pooling='max', input_shape=(224, 224, 3))
+		self.model = Model(inputs=vgg16.input, outputs=vgg16.get_layer('fc2').output)
+		
+	def get_feature_vector(self, img):
+		img = cv2.resize(img, (224, 224))
+		feature_vector = self.model.predict(img.reshape(1, 224, 224, 3))
+		return feature_vector
+	
+	def calculate_similarity(self, img1, img2):
+		vector1 = self.get_feature_vector(img1)
+		vector2 = self.get_feature_vector(img2)
+		return 1 - cosine(vector1, vector2)
+
+	def similarity(self, pred, labels):
+		sim = 0
+		for i in range(pred.shape[0]):
+			sim += self.calculate_similarity(pred[i], labels[i])
+		return sim / pred.shape[0]
+
+	def test(self, model, test_input, test_labels):
+		sim = 0
+		loss = 0
+		i = 0
+		while i + model.batch_size <= test_input.shape[0]:
+			batch_input = test_input[i: i+model.batch_size]
+			batch_label = test_labels[i: i+model.batch_size]
+			pred = model.call(batch_input)
+			loss += model.loss_function(pred, batch_label)
+			sim += self.similarity(pred, batch_label)
+			i += model.batch_size
+		return np.exp(loss * model.batch_size / i), sim * model.batch_size / i
+
+# if __name__ == '__main__':
+# 	model = evaluation()
+
+# 	img1 = cv2.imread("./img1.jpg")
+# 	img2 = cv2.imread("./img2.jpg")
+# 	img3 = cv2.imread("./img3.jpg")
+# 	img4 = cv2.imread("./img4.jpg")
+# 	imgs = np.array([img1, img2, img3, img4])
+# 	labels = np.array([img1, img1, img1, img1])
+	
+# 	print(model.calculate_similarity(img1, img1))
+# 	print(model.calculate_similarity(img1, img2))
+# 	print(model.calculate_similarity(img1, img3))
+# 	print(model.calculate_similarity(img1, img4))
+# 	print(model.similarity(imgs, labels))
